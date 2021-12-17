77bec5e1d975c645fdfc20e7f22ae050daf3586e
cigot <c.bensi.igot@gmail.com>
2021-12-17 00:16:49 -0500

    updated react post build

--- frontend/stampy/postbuild.sh
+++ frontend/stampy/postbuild.sh
@@ -10,6 +10,9 @@ awk '{sub("src=\"/static/","src=\"")}1' ./build/temp.html > ./build/temp2.html
 awk '{sub("src=\"/static/","src=\"")}1' ./build/temp2.html > ./build/temp.html
 awk '{sub("<!doctype html>","<%@ page language=\"java\" contentType=\"text/html; charset=ISO-8859-1\" pageEncoding=\"ISO-8859-1\"%><!DOCTYPE html>")}1' ./build/temp.html > ./build/stampyReact.jsp
 
+rm ./build/static/js/*.map
+rm ./build/static/css/*.map
+
 cp -r -f ./build/static/* ../../backend/stampy/src/main/resources/static
 cp -f ./build/asset-manifest.json ../../backend/stampy/src/main/resources/static
 cp -f ./build/favicon.ico ../../backend/stampy/src/main/resources/static
