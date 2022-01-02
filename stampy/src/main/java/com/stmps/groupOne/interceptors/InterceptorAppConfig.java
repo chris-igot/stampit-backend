2f88246425ea1e9375969a4bb3c033d4c07f548e
cigot <c.bensi.igot@gmail.com>
2022-01-02 05:10:55 -0500

    Added login interceptor for API

--- stampy/src/main/java/com/stmps/groupOne/interceptors/InterceptorAppConfig.java
+++ stampy/src/main/java/com/stmps/groupOne/interceptors/InterceptorAppConfig.java
@@ -9,9 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorAppConfig implements WebMvcConfigurer {
 	   @Autowired
 	   LoginInterceptor loginInterceptor;
+	   @Autowired
+	   LoginAPIInterceptor loginAPIInterceptor;
 
 	   @Override
 	   public void addInterceptors(InterceptorRegistry registry) {
-	      registry.addInterceptor(loginInterceptor).excludePathPatterns("/","/login","/register","/api/login","/api/register","/css/*");
+	      registry.addInterceptor(loginInterceptor).excludePathPatterns("/login","/register","/logout","/api/*","/css/*");
+	      registry.addInterceptor(loginAPIInterceptor).excludePathPatterns("/","/api/login","/api/register","/api/logout");
 	   }
 }
---
+++ stampy/src/main/java/com/stmps/groupOne/interceptors/LoginAPIInterceptor.java
@@ -0,0 +1,25 @@
+package com.stmps.groupOne.interceptors;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoginAPIInterceptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+		Long userId = (Long) request.getSession().getAttribute("id");
+//		String profileId = (String) request.getSession().getAttribute("profile_id");
+
+		if(userId == null) {
+			System.out.println("not logged in!");
+			response.sendError(401, "Must log in first");
+		} 
+		
+		return true;
+	}
+}
