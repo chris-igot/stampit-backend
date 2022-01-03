2958ce12ca102b00139794108f2ecea7cbb7bbf6
cigot <c.bensi.igot@gmail.com>
2022-01-03 07:25:04 -0500

    Added limit to last 1000

--- stampy/src/main/java/com/stmps/groupOne/repositories/StampRepository.java
+++ stampy/src/main/java/com/stmps/groupOne/repositories/StampRepository.java
@@ -10,6 +10,6 @@ import com.stmps.groupOne.models.Stamp;
 
 public interface StampRepository extends CrudRepository<Stamp,String>{
 	List<Stamp> findAll();
-	@Query(value = "SELECT * FROM stamps WHERE post_id = :postId", nativeQuery = true)
+	@Query(value = "SELECT * FROM stamps WHERE post_id = :postId ORDER BY created_at LIMIT 1000", nativeQuery = true)
 	List<Stamp> findStampsByPost(@Param("postId") String postId);
 }
