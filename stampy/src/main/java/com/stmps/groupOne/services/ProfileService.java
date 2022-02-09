571fa6bad3291c10eb81cda2ebd6ef0de5b7123f
cigot <c.bensi.igot@gmail.com>
2022-02-09 15:28:31 -0500

    Added dedicate follow data model

--- stampy/src/main/java/com/stmps/groupOne/services/ProfileService.java
+++ stampy/src/main/java/com/stmps/groupOne/services/ProfileService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.stmps.groupOne.models.FileEntry;
+import com.stmps.groupOne.models.Follow;
 import com.stmps.groupOne.models.Profile;
+import com.stmps.groupOne.repositories.FollowRepository;
 import com.stmps.groupOne.repositories.ProfileRepository;
 
 @Service
@@ -17,13 +19,15 @@ public class ProfileService {
 	ProfileRepository profileRepo;
 	@Autowired
 	JointFileService fileServ;
+	@Autowired
+	FollowRepository followRepo;
 	
 	public Profile add(Profile profile) {
 		return profileRepo.save(profile);
 	}
 	
 	public void addImage(MultipartFile uploadedFile, Profile profile) {
-		FileEntry image = fileServ.addImage(uploadedFile, "profile");
+		FileEntry image = fileServ.addImage(uploadedFile, "image");
 		profile.setImage(image);
 		profileRepo.save(profile);
 	}
@@ -38,5 +42,29 @@ public class ProfileService {
 		} else {			
 			return profileRepo.searchprofilenames(search);
 		}
+	}
+	
+	public void follow(String ownProfileId, String otherProfileId) {
+		Profile ownProfile = this.getById(ownProfileId);
+		Profile otherProfile = this.getById(otherProfileId);
+
+		if (otherProfile.getUser().hasRole("private")) {
+			followRepo.save(new Follow(ownProfile.getId(), otherProfile.getId(), false));
+		} else {
+			followRepo.save(new Follow(ownProfile.getId(), otherProfile.getId(), true));
+		}
+	}
+	
+	public void unfollow(String ownProfileId, String otherProfileId) {
+		Profile ownProfile = this.getById(ownProfileId);
+
+		for (Profile profile : ownProfile.getAmFollowing()) {
+			if(profile.getId().equals(otherProfileId)) {
+				ownProfile.getAmFollowing().remove(profile);
+				break;
+			}
+		}
+		
+		this.add(ownProfile);
 	}
 }
