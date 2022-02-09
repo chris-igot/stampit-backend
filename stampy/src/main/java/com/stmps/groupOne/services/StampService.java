8d193b50018eac63847b9dff71f12966be8bd3c1
cigot <c.bensi.igot@gmail.com>
2022-02-09 15:39:11 -0500

    Fixed stamp getter

--- stampy/src/main/java/com/stmps/groupOne/services/StampService.java
+++ stampy/src/main/java/com/stmps/groupOne/services/StampService.java
@@ -9,20 +9,23 @@ import com.stmps.groupOne.models.FileEntry;
 import com.stmps.groupOne.models.Post;
 import com.stmps.groupOne.models.Profile;
 import com.stmps.groupOne.models.Stamp;
+import com.stmps.groupOne.repositories.FileEntryRepository;
 import com.stmps.groupOne.repositories.StampRepository;
 
 @Service
 public class StampService {
 	@Autowired
 	StampRepository stampRepo;
+	@Autowired
+	FileEntryRepository fileRepo;
 	
 	public Stamp add(Profile profile, FileEntry image, Post post, Integer x, Integer y) {
 		Stamp stamp = new Stamp(profile,image,post,x,y);
 		return stampRepo.save(stamp);
 	}
 	
-	public List<Stamp> getAllStamps() {
-		return stampRepo.findAll();
+	public List<FileEntry> getAllStamps() {
+		return fileRepo.findByCategoryEquals("stamp");
 	}
 	
 	public List<Stamp> getPostStamps(String postId) {
