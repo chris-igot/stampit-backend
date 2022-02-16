90403cfb736db2f90ea9e9f0d4c9aa7eb44966be
cigot <c.bensi.igot@gmail.com>
2022-02-16 04:44:49 -0500

    Added missing filed in native query

--- stampy/src/main/java/com/stmps/groupOne/repositories/PostRepository.java
+++ stampy/src/main/java/com/stmps/groupOne/repositories/PostRepository.java
@@ -12,9 +12,9 @@ public interface PostRepository extends CrudRepository<Post, String> {
 	List<Post> findAll();
 	List<Post> findAllByOrderByCreatedAtDesc();
 	
-	@Query(value = "SELECT posts.id as id, posts.image_id as image_id, posts.profile_id as profile_id, posts.created_at as created_at, posts.updated_at as updated_at FROM posts INNER JOIN follows ON posts.profile_id = follows.them_id WHERE follows.you_id = :userId AND follows.follow_verified IS TRUE ORDER BY posts.created_at DESC;", nativeQuery = true)
+	@Query(value = "SELECT posts.id as id, posts.image_id as image_id, posts.description as description, posts.profile_id as profile_id, posts.created_at as created_at, posts.updated_at as updated_at FROM posts INNER JOIN follows ON posts.profile_id = follows.them_id WHERE follows.you_id = :userId AND follows.follow_verified IS TRUE ORDER BY posts.created_at DESC;", nativeQuery = true)
 	List<Post> findAllFollowed(@Param("userId") String userId);
 
-	@Query(value = "SELECT posts.id as id, posts.image_id as image_id, posts.profile_id as profile_id, posts.created_at as created_at, posts.updated_at as updated_at FROM posts INNER JOIN profiles ON posts.profile_id = profiles.id WHERE profiles.is_private IS NOT TRUE ORDER BY posts.created_at DESC;", nativeQuery = true)
+	@Query(value = "SELECT posts.id as id, posts.image_id as image_id, posts.description as description, posts.profile_id as profile_id, posts.created_at as created_at, posts.updated_at as updated_at FROM posts INNER JOIN profiles ON posts.profile_id = profiles.id WHERE profiles.is_private IS NOT TRUE ORDER BY posts.created_at DESC;", nativeQuery = true)
 	List<Post> findAllPublic();
 }
